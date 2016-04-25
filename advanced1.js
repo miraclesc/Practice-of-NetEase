94d6a3885ba0fc5b940efac30a3bde3f49c005b7
Shi Chong <miracle_sc@163.com>
2016-04-25 20:45:03 +0800

    Update advanced1.js

--- advanced1.js
+++ advanced1.js
@@ -1,3 +1,19 @@
+//circle
+		function Circle(r){
+			this.r=r;
+		}
+		Circle.prototype.pre= function() {
+			return (2*Math.PI*this.r).toFixed(2);
+		}
+		Circle.prototype.area= function() {
+			return (Math.PI*Math.pow(this.r,2)).toFixed(2);
+		};
+		var circle=new Circle(6);
+		console.log(circle.pre());
+		console.log(circle.area());
+
+
+
 //test1
 function type(obj){
   		var types= Object.prototype.toString.call(obj).slice(8,-1)
